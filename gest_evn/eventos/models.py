577f479abe51bcffeb226d86c09627c392bd2c4a
jesus <normales01@gmail.com>
2024-09-09 18:27:42 -0500

    #correccion - models
    Se corrigio el problema relacionado con el ForeignKey, se añadio el parametro de on_delete para que django sepa que hacer en caso que sea eliminada.

--- gest_evn/eventos/models.py
+++ gest_evn/eventos/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 
-
+class Organizador(models.Model):
+    nombre = models.CharField(max_length=30)
 
 class Evento(models.Model):
     nombre = models.CharField(max_length=30)
-    Organizador= models.ForeignKey(Organizador)
-    fecha =  model.DateField()
+    Organizador= models.ForeignKey(Organizador, on_delete=models.CASCADE)
+    fecha =  models.DateField()
 
-class Organizador(models.Model):
-    nombre = models.CharField(max_length=30)
+
 
