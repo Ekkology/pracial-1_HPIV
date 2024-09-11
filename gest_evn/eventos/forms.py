c5bacfb627b863654202467b97f7e71b1762f4d3
jesus <normales01@gmail.com>
2024-09-11 17:07:44 -0500

    Diseño del  Forms
    Forms basico para crear los eventos.

---
+++ gest_evn/eventos/forms.py
@@ -0,0 +1,12 @@
+from django.forms import ModelForm
+from .models import Evento
+
+
+class EventoForm(ModelForm):
+     class Meta:
+        model = Evento
+        fields = ["nombre","Organizador","fecha"]
+
+
+
+form = EventoForm()
