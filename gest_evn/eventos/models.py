eb4bfe79bb12805e8a8ad8c8b771aa5d22c8b8c5
jesus <normales01@gmail.com>
2024-09-11 20:27:46 -0500

    Corrección De Nombres
    Se corrigio el nombre con el que eran llamado los objetos de las tablas.

--- gest_evn/eventos/models.py
+++ gest_evn/eventos/models.py
@@ -2,11 +2,14 @@ from django.db import models
 
 class Organizador(models.Model):
     nombre = models.CharField(max_length=30)
+    def __str__(self):
+        return self.nombre
 
 class Evento(models.Model):
     nombre = models.CharField(max_length=30)
     Organizador= models.ForeignKey(Organizador, on_delete=models.CASCADE)
     fecha =  models.DateField()
+    
 
 
 
