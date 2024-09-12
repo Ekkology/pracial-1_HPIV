bc7f22ced3eba06fd785a0d371126b2c097764e5
jesus <normales01@gmail.com>
2024-09-11 20:26:37 -0500

    Mejor Formato
    Se mejoro el formato en el que el sistema pide las fechas de los eventos

--- gest_evn/eventos/forms.py
+++ gest_evn/eventos/forms.py
@@ -1,11 +1,16 @@
 from django.forms import ModelForm
 from .models import Evento
+from django import forms
+
 
 
 class EventoForm(ModelForm):
      class Meta:
         model = Evento
         fields = ["nombre","Organizador","fecha"]
+        widgets = {
+            'fecha': forms.DateInput(attrs={'type': 'date'})  # Usa el widget de entrada de fecha
+        }
 
 
 
