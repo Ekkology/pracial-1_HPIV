b29b18a5823fa12fbaf2b3bae5b3401c2cafa07e
jesus <normales01@gmail.com>
2024-09-11 20:29:05 -0500

    Mejor organización
    Se agregaron rutas exclusivas de la app de eventos, para hacer CRU, de las tablas.

--- gest_evn/eventos/urls.py
+++ gest_evn/eventos/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
-from .views import lista_home
+from .views import OrganizadorListView, OrganizadorCreateView
+from . import views
 
 urlpatterns = [
-    path('eventos/', lista_home , name='lista_eventos'),
+    path('eventos/', views.listar_eventos, name='listar_eventos'),
+    path('eventos/crear/', views.crear_evento, name='crear_evento'),
+    path('eventos/editar/<int:pk>/', views.editar_evento, name='editar_evento'),
+    path('organizadores/', OrganizadorListView.as_view(), name='organizador-list'),
+    path('organizadores/nuevo/', OrganizadorCreateView.as_view(), name='organizador-create'),
 ]
 
